95487804d5dc052c6c15ae37df4de88b6085bb92
Ayush Basnet <ayushjung63@gmail.com>
2022-04-10 21:25:00 +0545

    java version 8

--- src/main/java/com/ayush/proms/exception/ProjectExceptionHandler.java
+++ src/main/java/com/ayush/proms/exception/ProjectExceptionHandler.java
@@ -2,6 +2,8 @@ package com.ayush.proms.exception;
 
 import com.ayush.proms.exception.project.ProjectStatusNotValidException;
 import com.ayush.proms.utils.BaseController;
+import com.ayush.proms.utils.CustomMessageSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,14 +11,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Map;
 
 @RestController
 @ControllerAdvice
 public class ProjectExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    private CustomMessageSource customMessageSource;
+
     @ExceptionHandler(ProjectStatusNotValidException.class)
     public ResponseEntity projectStatusNotValidException(ProjectStatusNotValidException exception){
-        return new ResponseEntity(Map.of("message",exception.getMessage()), HttpStatus.OK);
+        return ResponseEntity.ok(customMessageSource.get("not.foung",customMessageSource.get("project")));
     }
 }
